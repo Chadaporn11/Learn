ae4ca49a407e10c9c39a239fe8ff208e08fca13d
Chadaporn Thaikana <thaikana2544@gmail.com>
2022-11-11 00:08:14 +0700

    test2

---
+++ typeScript-Basic/dist/SpeadOperator.js
@@ -0,0 +1,6 @@
+"use strict";
+//node SpeadOperator.js
+const section = ['บัญชี', 'การเงิน'];
+const department = ['โปรเเกรมเมอร์', 'การตลาด', 'กราฟิก'];
+department.push(...section);
+console.log(department);
--- typeScript-Basic/src/Func.ts
+++ typeScript-Basic/src/Func.ts
@@ -59,6 +59,39 @@ function randomPosition():{lat:number,long:number}{
 }
 console.log(randomPosition());*/
 
+//func overloading
+//#1
+/*function sayHi():string
+function sayHi(name:string):string
+function sayHi(name?:unknown):unknown{
+    if(!name){
+        return 'Hello 1'
+    }
+    if(typeof name == 'string'){
+        return 'Hello '+name
+    }
+    throw new Error('Type error')
+}
+console.log(sayHi())
+console.log(sayHi('som'))*/
+//#2
+function total(a:number,b:number):number
+function total(a:string,b:string):string
+function total(a:unknown,b:unknown):unknown{
+    if(typeof a == 'number' && typeof b == 'number'){
+        return a+b
+    }
+    if(typeof a == 'string' && typeof b == 'string'){
+        return parseInt(a)+parseInt(b)
+    }
+    throw new Error('Type error')
+}
+
+console.log(total(10,20))
+console.log(total('50','60'))
+
+
+
 
 
 
